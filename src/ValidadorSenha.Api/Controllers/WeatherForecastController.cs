d05a31f343a2317f1c1d504debcca3dcb76384ba
William P. Pinto <william.pinto@lambda3.com.br>
2020-03-08 14:22:41 -0300

    Adicionado e configurado Swagger; criado primeiro teste de API para comportamento inicializado pelo scaffolding

--- src/ValidadorSenha.Api/Controllers/WeatherForecastController.cs
+++ src/ValidadorSenha.Api/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ValidadorSenha.Api.Controllers.v1
+namespace ValidadorSenha.Api.Controllers
 {
     [ApiController]
     [ApiVersion("1.0")]
--- src/ValidadorSenha.Api/Startup.cs
+++ src/ValidadorSenha.Api/Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace ValidadorSenha.Api
 {
@@ -21,6 +24,8 @@ namespace ValidadorSenha.Api
         {
             services.AddControllers();
 
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
             services.AddApiVersioning(options =>
             {
                 options.DefaultApiVersion = new ApiVersion(1, 0);
@@ -33,21 +38,29 @@ namespace ValidadorSenha.Api
                 options.SubstituteApiVersionInUrl = true;
                 options.GroupNameFormat = "'v'VV";
             });
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                // build a swagger endpoint for each discovered API version
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
