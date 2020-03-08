fbd5adcc5420c31338adecc2db4d4cc0ba8ab196
William P. Pinto <william.pinto@lambda3.com.br>
2020-03-08 12:20:49 -0300

    Adicionando pasta para versionamento da controller

--- src/ValidadorSenha.Api/Controllers/v1/WeatherForecastController.cs
+++ src/ValidadorSenha.Api/Controllers/v1/WeatherForecastController.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ValidadorSenha.Api.Controllers
+namespace ValidadorSenha.Api.Controllers.v1
 {
     [ApiController]
     [ApiVersion("1.0")]
